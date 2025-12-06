e309067f0b5257331c2164b7dfffbd00b16ec8f9
josecorrea01 <josecorrea.electricidad@gmail.com>
2025-12-05 23:53:52 -0300

    chore: guarda cambios locales WIP en SkillTrack

--- src/main/java/com/jcorrea/skilltrack_empresas/repository/ColaboradorRepository.java
+++ src/main/java/com/jcorrea/skilltrack_empresas/repository/ColaboradorRepository.java
@@ -3,6 +3,9 @@ package com.jcorrea.skilltrack_empresas.repository;
 import com.jcorrea.skilltrack_empresas.model.Colaborador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColaboradorRepository extends JpaRepository<Colaborador, Integer> {
-    // Por ahora solo usamos los métodos básicos (findAll, findById, etc.)
+import java.util.List;
+
+public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
+
+    List<Colaborador> findByEmpresa_IdEmpresa(Long idEmpresa);
 }
