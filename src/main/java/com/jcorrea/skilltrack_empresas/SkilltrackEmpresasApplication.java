0854bfacfd06de720d70d3fd616dfadf478cd716
josecorrea01 <josecorrea.electricidad@gmail.com>
2025-12-07 23:13:05 -0300

    Configurar Skilltrack-Empresas como WAR ejecutable con soporte JSP y activar data-railway.sql para H2 en Railway

--- src/main/java/com/jcorrea/skilltrack_empresas/SkilltrackEmpresasApplication.java
+++ src/main/java/com/jcorrea/skilltrack_empresas/SkilltrackEmpresasApplication.java
@@ -2,12 +2,19 @@ package com.jcorrea.skilltrack_empresas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SkilltrackEmpresasApplication {
+public class SkilltrackEmpresasApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkilltrackEmpresasApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // Para despliegue tradicional (WAR en un Tomcat externo) o embebido
+        return builder.sources(SkilltrackEmpresasApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SkilltrackEmpresasApplication.class, args);
+    }
 }
